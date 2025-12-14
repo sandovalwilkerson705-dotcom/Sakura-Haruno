cfa09c4a1305ee3de7879e2d0589d82f2f60d27f
sandovalwilkerson705-dotcom <sandovalwilkerson705@gmail.com>
2025-12-13 22:31:47 -0400

    Actualizar Serbotfalso.js

--- plugins/Serbotfalso.js
+++ plugins/Serbotfalso.js
@@ -8,15 +8,15 @@ const handler = async (msg, { conn, command }) => {
   const texto = `
 🚧 *Sistema en desarrollo...* 🚧
 
-💡 El sistema de *subbots* aún no está disponible en *La Suki Bot*.  
+💡 El sistema de *subbots* aún no está disponible en *Sakura Haruno*.  
 Estamos trabajando en ello para que funcione perfectamente.  
 🕒 Estimamos que tomará entre *2 a 3 meses más* para estar listo.
 
-✨ Gracias por tu paciencia y por ser parte del desarrollo de *La Suki Bot* 💖
+✨ Gracias por tu paciencia y por ser parte del desarrollo de *Sakura Haruno* 💖
 `.trim();
 
   await conn.sendMessage(chatId, {
-    image: { url: "https://cdn.russellxz.click/37bd2805.jpeg" },
+    image: { url: "https://cdn.russellxz.click/5615db7e.jpg" },
     caption: texto
   }, { quoted: msg });
 };
