ce5f0b38670fe93a65b0f469aa24e8108032d1da
sandovalwilkerson705-dotcom <sandovalwilkerson705@gmail.com>
2025-12-14 09:18:12 -0400

    Actualizar Todos.js

--- plugins/pluginsgrupos/Todos.js
+++ plugins/pluginsgrupos/Todos.js
@@ -77,7 +77,7 @@ const handler = async (msg, { conn, args }) => {
     const extraMsg = (args || []).join(" ");
 
     let finalMsg  = `╭─⌈ 🔊 𝐓𝐀𝐆𝐀𝐋𝐋 𝐌𝐎𝐃𝐄 ⌋──╮\n`;
-        finalMsg += `│ 🤖 *✧ Sᵘᵏⁱ 3.0 ᴮᵒᵗ ✧*\n`;
+        finalMsg += `│ 🤖 *Sakura Haruno*\n`;
         finalMsg += `│ 👤 *Invocador:* @${senderDigits}\n`;
     if (extraMsg.length > 0) {
         finalMsg += `│ 💬 *Mensaje:* ${extraMsg}\n`;
@@ -87,7 +87,7 @@ const handler = async (msg, { conn, args }) => {
         finalMsg += mentionList;
 
     await conn.sendMessage(chatId, {
-      image: { url: "https://cdn.russellxz.click/034af9ef.jpeg" },
+      image: { url: "https://cdn.russellxz.click/012aac15.jpg" },
       caption: finalMsg,
       mentions: mentionIds
     }, { quoted: msg });
